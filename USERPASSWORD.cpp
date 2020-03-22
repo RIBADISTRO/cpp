42fe5cf77a4e38a582029acb2597c9d9747655c6
RIBADISTRO <ribali0.96@gmail.com>
2020-03-22 00:10:46 -0600

    Nombre de fichero cambiado

--- USERPASSWORD.cpp
+++ USERPASSWORD.cpp
@@ -6,39 +6,42 @@
 #define KEY "iOS"
 using namespace std;
 
-
 int main()
 {
-	string usuario,password;
-	int contador=0;
-	bool ingresar=false;
-	do{ 
-	system("clear");
-	cout<<"\t\tLOGING DE usuario"<<endl;
-	cout<<"\t\t-----------------"<<endl;
-	cout<<"\t\tusuario: ";
-	getline(cin, usuario);
-	cout<<"\t\tpassword: ";
-	getline(cin,password);
+	string usuario, password;
+	int contador = 0;
+	bool ingresar = false;
+	do
+	{
+		system("clear");
+		cout << "\t\tLOGING DE usuario" << endl;
+		cout << "\t\t-----------------" << endl;
+		cout << "\t\tusuario: ";
+		getline(cin, usuario);
+		cout << "\t\tpassword: ";
+		getline(cin, password);
 
-	if (usuario == USER && password == KEY){
-		ingresar = true;
+		if (usuario == USER && password == KEY)
+		{
+			ingresar = true;
 		}
-	else{
-		cout<<"El usuario y/o password son incorrecto"<<endl;
-		cin.get();
-		contador++;
-	}
+		else
+		{
+			cout << "El usuario y/o password son incorrecto" << endl;
+			cin.get();
+			contador++;
+		}
 
-	} 
-	while ( ingresar == false && contador < 3);
+	} while (ingresar == false && contador < 3);
 	cin.get();
-	
+
 	if (ingresar == false)
 	{
-		cout<<"Usted no pudeo ingresar....!"<<endl;
-	}else{
-		cout<<"BIENVENIDO\n, Seccion iniciada"<<endl;
+		cout << "Usted no pudeo ingresar....!" << endl;
+	}
+	else
+	{
+		cout << "BIENVENIDO\n, Seccion iniciada" << endl;
 	}
 	return 0;
 }
