c0790462c0b849bb4e30779ff659474e811402f5
RIBADISTRO <ribali0.96@gmail.com>
2020-09-14 13:43:48 -0500

    Modifique lineas de funciones

--- MetodosNumericos/MetodoBiseccion.cpp
+++ MetodosNumericos/MetodoBiseccion.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <cmath>
 #include <iomanip>
-#define LIMITACION 10
+// #define LIMITACION 5
 
 using namespace std;
 
@@ -10,12 +10,18 @@ using namespace std;
  * =                METODO DE BISECCION                 =
  **======================================================*/
 
-float f(float x );
-float funcion;
+double f(double x );
+double biseccion(double xi, double xu, double tol, double imax);
+double funcion;
 int main(){
-
-    float xi, xu, xr, fxi, fxr, pm,error;
-    int  iter=0,imax;
+    /** Paso nuemro 1:
+     *  Conocer las dos varibles de entrada del intervado cerrado
+     *  [ xi, xu]
+     *  Luego l
+     * 
+     */
+    double xi, xu, tol, raiz;
+    int imax;
 
     cout<<"\n\t\e[36m=================================================\e[0m\n";
     cout<<"\t\e[36m=          METODO DE BISECCION                  =\e[0m\n";
@@ -26,28 +32,36 @@ int main(){
 
     cout<<"Valor de [\e[44mXi\e[0m, \e[41mXu\e[0m ]:";      cin>>xi>>xu;
     cout<<"\n";
+    cout<<"Tolerancia: ";                                   cin>>tol;
     cout<<"Cuantas iteraciones: ";                          cin>>imax;
-    cout<<"\e[96m|it\t|Xi\t\t|Xu\t\t|Xr\t\t|f(Xi)\t\t\t|f(Xr)\t\t|f(Xi) f(Xr)\t\tt|Error\n\e[0m";
-    cout<<"|--------------------------------------------------------------------------------------------------------------------------------------|\n";
-
-    do{ 
-        fxi = f(xi);
-        fxr = f(xr);
-        /* Paso numero 1*/
-        xr = (xi+xr)/2;
-        if(){
-
-        }
-        cout<<setprecision(LIMITACION)<<"| "<<iter<<"\t| "<<xi<<"    \t\t"<<xu<<"\t\t"<<xr<<"\t\t"<<fxi<<"\t\t"<<fxr<<endl; 
-        iter++;
-    }while (iter<=imax);
     
-
-
+    raiz = biseccion(xi,xu,tol,imax);
+    cout<<"La raiz es: "<<raiz<<endl;    
+    
     return 0;
 }
 
-float f(float x ){
+double biseccion(double xi, double xu, double tol, double imax){
+    double xr;
+    int iter=0;
+    cout<<"\e[96m| it\t| Xi\t\t| Xu\t\t| Xr\t\t| f(Xi)\t\t| f(Xr)\t\t| f(Xi) * f(Xr)\t\t| Error\n\e[0m";
+    cout<<"|--------------------------------------------------------------------------------------------------------------------------------------|\n";
+    
+    do{ 
+       xr = (xi + xu)/2;
+       if(f(xi) * f(xr)<0){
+           xu = xr;
+       }else{
+           xi= xr;
+       }
+
+        cout<<"\t| "<<iter<<"\t| "<<xi<<"\t\t| "<<xu<<"\t\t| "<<xr<<"\t\t| "<<f(xi)<<"\t\t| "<<f(xr)<<endl; 
+        iter++;
+    }while ((abs(f(xr))>tol) && (iter < imax));
+    return xr;   
+}
+
+double f(double x ){
 	    funcion=cos(x)-x;
     return  funcion;
 }
